07a3b91bb03e40061f7e99b628e5a649b3321e65
Willem Gillis <willem.gillis@gmail.com>
2022-04-25 19:51:44 +0200

    Serialization example

--- Models/Auth/AuthMsg.cs
+++ Models/Auth/AuthMsg.cs
@@ -1,88 +1,90 @@
 namespace example
 {
   using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Text.RegularExpressions;
+  using System.Text.Json.Serialization;
+  using System.Text.RegularExpressions;
+
   [JsonConverter(typeof(AuthMsgConverter))]
-public class AuthMsg {
-  private Type type;
+  public class AuthMsg
+  {
+    private int? type;
 
-  public Type Type 
-  {
-    get { return type; }
-    set { type = value; }
-  }
-}
-
-internal class AuthMsgConverter : JsonConverter<AuthMsg>
-{
-  public override bool CanConvert(System.Type objectType)
-  {
-    // this converter can be applied to any type
-    return true;
-  }
-  public override AuthMsg Read(ref Utf8JsonReader reader, System.Type typeToConvert, JsonSerializerOptions options)
-  {
-    if (reader.TokenType != JsonTokenType.StartObject)
+    public int? Type 
     {
-      throw new JsonException();
+      get { return type; }
+      set { type = value; }
     }
+  }
 
-    var instance = new AuthMsg();
-  
-    while (reader.Read())
+  internal class AuthMsgConverter : JsonConverter<AuthMsg>
+  {
+    public override bool CanConvert(System.Type objectType)
     {
-      if (reader.TokenType == JsonTokenType.EndObject)
-      {
-        return instance;
-      }
-
-      // Get the key.
-      if (reader.TokenType != JsonTokenType.PropertyName)
+      // this converter can be applied to any type
+      return true;
+    }
+    public override AuthMsg Read(ref Utf8JsonReader reader, System.Type typeToConvert, JsonSerializerOptions options)
+    {
+      if (reader.TokenType != JsonTokenType.StartObject)
       {
         throw new JsonException();
       }
 
-      string propertyName = reader.GetString();
-      if (propertyName == "type")
+      var instance = new AuthMsg();
+  
+      while (reader.Read())
       {
-        var value = TypeExtension.ToType(JsonSerializer.Deserialize<dynamic>(ref reader, options));
-        instance.Type = value;
-        continue;
+        if (reader.TokenType == JsonTokenType.EndObject)
+        {
+          return instance;
+        }
+
+        // Get the key.
+        if (reader.TokenType != JsonTokenType.PropertyName)
+        {
+          throw new JsonException();
+        }
+
+        string propertyName = reader.GetString();
+        if (propertyName == "type")
+        {
+          var value = JsonSerializer.Deserialize<int?>(ref reader, options);
+          instance.Type = value;
+          continue;
+        }
+
+    
+
+    
+      }
+  
+      throw new JsonException();
+    }
+    public override void Write(Utf8JsonWriter writer, AuthMsg value, JsonSerializerOptions options)
+    {
+      if (value == null)
+      {
+        JsonSerializer.Serialize(writer, null);
+        return;
+      }
+      var properties = value.GetType().GetProperties();
+  
+      writer.WriteStartObject();
+
+      if(value.Type != null) { 
+        // write property name and let the serializer serialize the value itself
+        writer.WritePropertyName("type");
+        JsonSerializer.Serialize(writer, value.Type);
       }
 
-    
 
-    
-    }
   
-    throw new JsonException();
+
+  
+
+      writer.WriteEndObject();
+    }
+
   }
-  public override void Write(Utf8JsonWriter writer, AuthMsg value, JsonSerializerOptions options)
-  {
-    if (value == null)
-    {
-      JsonSerializer.Serialize(writer, null);
-      return;
-    }
-    var properties = value.GetType().GetProperties();
-  
-    writer.WriteStartObject();
-
-    if(value.Type != null) { 
-      // write property name and let the serializer serialize the value itself
-      writer.WritePropertyName("type");
-      JsonSerializer.Serialize(writer, value.Type.GetValue());
-    }
-
-
-  
-
-  
-
-    writer.WriteEndObject();
-  }
-
-}
 
 }
